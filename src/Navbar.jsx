bd0a26ffa55a816b5d9c22d3d52e0b94e42ae117
Minlync <93282772+Minlync@users.noreply.github.com>
2026-02-12 21:04:35 -0500

    style changed
    added logo and fonts

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,12 @@
 
+import logo from "./assets/dicelogo.png";
+
 
 export default function Navbar() {
     return (
      <nav className="nav">
-        <a href="/" className="sitename"> 
-        Dice
+        <a href="/" className="sitename">
+       <img src={logo} alt="Dice Logo" width="120" height="50" style={{ objectFit: "contain" }}  className="logo" />
         </a>
         <ul>
             <li>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
 import App from './App';
+import "@fontsource/roboto";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
